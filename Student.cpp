65ced1efc5c82c979e23c1591b36a97ad3d0fb59
IT21381768 <99156886+IT21381768@users.noreply.github.com>
2022-05-14 16:21:26 +0000

    done

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,15 @@
 #include "Student.h"
 #include <iostream>
-
+#include <cstring>
+using namespace std;
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int sId, const char n[]) {
+  studentId=sId;
+  strcpy(name,n);
 }
-
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display()
+{
+  cout <<"Student ID is"<<studentId<<endl;
+  cout <<"Name is "<<name<<endl;
 }
--- Student.h
+++ Student.h
@@ -1,9 +1,14 @@
 class Student {
   // private section
+private:
   //    int studentId
+int studentId;
   //    name <- 20 charcters
-  
+char name[20]; 
   // public section
+public:
   //    assignDetails() method declaration
+void assignDetails(int sid,const char n[]);
   //    display() method declaration
+void display();
 };
